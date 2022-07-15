13c86e50e3de7e67df43e5be6c6ac9f8ed495b94
Narvin <narvinp16@gmail.com>
2022-07-15 12:40:55 +0530

    https://github.com/narvin-05/Disney-Hotstar-Clone.git

--- src/component/Viewers.js
+++ src/component/Viewers.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import img1 from "../images/viewers-disney.png";
+import img2 from "../images/viewers-pixar.png";
+import img3 from "../images/viewers-marvel.png";
+import img4 from "../images/viewers-starwars.png";
+import img5 from "../images/viewers-national.png";
 
 
 function Viewers() {
@@ -10,16 +14,16 @@ function Viewers() {
             <img src={img1}/>
         </Wrap>
         <Wrap>
-            <img src={img1}/>
+            <img src={img2}/>
         </Wrap>
         <Wrap>
-            <img src={img1}/>
+            <img src={img3}/>
         </Wrap>
         <Wrap>
-            <img src={img1}/>
+            <img src={img4}/>
         </Wrap>
         <Wrap>
-            <img src={img1}/>
+            <img src={img5}/>
         </Wrap>
     </Container>
   )
@@ -36,15 +40,25 @@ const Container = styled.div`
 `
 
 const Wrap = styled.div`
-
+    cursor: pointer;
     border: 3px solid rgba(249,249,249,0.1);
     border-radius: 10px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
 
     img{
         width: 100%;
         height: 100px;
         object-fit: cover;
-
+    }
+    &:hover{
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+        rgb(0 0 0 / 73%) 0px 30px 22px -10px;
+        transform: scale(1.05);
+        border-color: white;
 
     }
 `
+
+// wrap:hover   = &:hover 
